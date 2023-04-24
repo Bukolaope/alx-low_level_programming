910d0c9830c11a490e4ff96d652d918b2a09c516
Bukola <oluwabukolaope@gmail.com>
2023-04-24 01:25:13 -0700

    Last Digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,6 @@
 int main(void)
 
 {
-	
 	int n, lastd;
 
 	srand(time(0));
@@ -34,5 +33,4 @@ int main(void)
 		printf("Last digit of %d is %d  and less than 6 and not 0\n", n, lastd);
 	}
 	return (0);
-
 }
